b73db066a57e8a1c6dcafde58a65f4699902670e
Blair Brown <blair.j.brown@gmail.com>
2021-12-14 22:17:04 -0500

    reformatted updated

--- Stressies.Data/Customers/CustomerRepository.cs
+++ Stressies.Data/Customers/CustomerRepository.cs
@@ -13,7 +13,16 @@ namespace Stressies.Data.Customers
         private string insertStatement = "INSERT INTO Customers (FirstName, LastName, Email, StreetAddress, StreetAddress2, City, State, Zip) VALUES (@FirstName, @LastName, @Email, @StreetAddress, @StreetAddress2, @City, @State, @Zip)";
         private string deleteStatement = "DELETE FROM Customers WHERE [CustomerID] = @CustomerID";
         private string getByIdStatement = "SELECT * FROM Customers WHERE CustomerID = @CustomerID";
-        private string updateStatement = "UPDATE Customers SET FirstName = @FirstName, LastName = @LastName, Email = @Email, StreetAddress = @StreetAddress, StreetAddress2 = @StreetAddress2, City = @City, State = @State, Zip = @Zip WHERE CustomerID = @CustomerID";
+        private string updateStatement = @"
+                UPDATE Customers SET FirstName = @FirstName, 
+                                     LastName = @LastName, 
+                                     Email = @Email, 
+                                     StreetAddress = @StreetAddress, 
+                                     StreetAddress2 = @StreetAddress2, 
+                                     City = @City, 
+                                     State = @State, 
+                                     Zip = @Zip 
+                               WHERE CustomerID = @CustomerID";
 
         public async Task<Customer> AddCustomer(Customer customer)
         {
@@ -46,6 +55,7 @@ namespace Stressies.Data.Customers
             {
                 await sqlconnection.ExecuteAsync(updateStatement, new { customerId = customer.CustomerID });
                 return customer;
+                //dont want to return what was sent in nomas... how do i return what was updated
 
             }
         }
